a43a7f3b06da93ea21e02df6f73b4e187def3018
Rayn Light <144638578+RaynLight@users.noreply.github.com>
2024-03-26 21:38:43 -0500

    Create Problem1.go

---
+++ Problem1.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Project Euler solve for problem #1
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+// we get 3,5,6 and 9. The sum of these multiples is 23.
+// Find the sum of all multiples of 3 or 5 below 1000
+
+func main() {
+	sum := 0
+	for x := 0; x < 1000; x++ {
+		if x%3 == 0 || x%5 == 0 {
+			sum += x
+		}
+	}
+
+	fmt.Println(sum)
+
+}
